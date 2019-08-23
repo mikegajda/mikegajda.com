4dc08e1da092027fdc5660903f1637dc381685f4
Mike Gajda <mgajda@bu.edu>
2019-08-22 22:24:24 -0400

    Set size of page to 15 articles (from 5)

--- gatsby-node.js
+++ gatsby-node.js
@@ -99,7 +99,7 @@ exports.createPages = ({ graphql, actions }) => {
             edges: posts,
             createPage: createPage,
             pageTemplate: 'src/templates/index.js',
-            pageLength: 5, // This is optional and defaults to 10 if not used
+            pageLength: 15, // This is optional and defaults to 10 if not used
             pathPrefix: '', // This is optional and defaults to an empty string if not used
             context: {}, // This is optional and defaults to an empty object if not used
           })
